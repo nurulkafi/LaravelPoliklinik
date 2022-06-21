cc86a20e1d46854ca837b515ab0cd4fcf751b1d9
nurulkafi <nurulkafi18@gmail.com>
2022-06-21 20:18:34 +0700

    auth api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -16,7 +16,8 @@ use Spatie\Permission\Models\Permission;
 class AuthController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $data = User::latest()->get();
         return response([
             'success' => true,
@@ -24,17 +25,19 @@ class AuthController extends Controller
             'data' => $data
         ], 200);
     }
-    public function store(request $request){
+    public function store(request $request)
+    {
         $name = $request->name;
         $email = $request->email;
         $password = $request->password;
-        $user = User::where('email',$email)->first();
+        $user = User::where('email', $email)->first();
         if (Auth::attempt(['email' => $email, 'password' => $password])) {
-            $pasien = Pasien::where('user_id',$user->id)->first();
+            $pasien = Pasien::where('user_id', $user->id)->first();
             return response([
                 'success' => true,
                 'message' => 'Success',
                 'id' => $user->id,
+                'name' => $user->name,
                 'pasien_id' => $pasien->id
             ], 200);
         }
@@ -42,40 +45,9 @@ class AuthController extends Controller
             'success' => false,
             'message' => 'failed',
         ], 200);
-        // return response([
-        //     'success' => false,
-        //     'message' => 'List Users',
-        // ], 200);
-        // $role = $request->role;
-
-        // $emailChecked = $this->emailChecked($email);
-        // if ($emailChecked == null) {
-        //     $saved = User::create([
-        //         'name' => $name,
-        //         'email' => $email,
-        //         'password' => bcrypt($password)
-        //     ]);
-
-        //     if ($saved) {
-        //         $saved->assignRole($role);
-        //         return response()->json([
-        //             'success' => true,
-        //             'message' => 'User Berhasil Disimpan!',
-        //         ], 200);
-        //     } else {
-        //         return response()->json([
-        //             'success' => false,
-        //             'message' => 'User Gagal Disimpan!',
-        //         ], 401);
-        //     }
-        // }else{
-        //     return response([
-        //         'success' => false,
-        //         'message' => 'Email Sudah Dipakai'
-        //     ], 401);
-        // }
     }
-    public function show($id){
+    public function show($id)
+    {
         $data = User::whereId($id)->first();
         return response([
             'success' => true,
@@ -84,7 +56,8 @@ class AuthController extends Controller
         ], 200);
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
         $user = User::findOrFail($id);
         $user->delete();
         if ($user) {
@@ -100,45 +73,30 @@ class AuthController extends Controller
         }
     }
 
-    public function update(request $request,$id){
+    public function update(request $request, $id)
+    {
         //
         $user = User::find($id);
-        $name = $request->name;
         $email = $request->email;
         $password = $request->password;
         $role = $request->role;
 
-        $array = [
-            'name' =>$name,
+        $saved = $user->update([
             'email' => $email,
             'password' => bcrypt($password)
-        ];
-        $emailChecked =$this->validatorDataUsers($array,$id);
-        if ($emailChecked->fails()) {
-            return response([
-                'success' => false,
-                'message' => $emailChecked->errors()
-            ], 401);
+        ]);
+        if ($saved) {
+            DB::table('model_has_roles')->where('model_id', $id)->delete();
+            $user->assignRole($role);
+            return response()->json([
+                'success' => true,
+                'message' => 'User Berhasil diubah!',
+            ], 200);
         } else {
-            $saved = $user->update([
-                'name' => $name,
-                'email' => $email,
-                'password' => bcrypt($password)
-            ]);
-            if ($saved) {
-                DB::table('model_has_roles')->where('model_id', $id)->delete();
-                $user->assignRole($role);
-                return response()->json([
-                    'success' => true,
-                    'message' => 'User Berhasil diubah!',
-                ], 200);
-            } else {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'User Gagal diubah!',
-                ], 401);
-            }
+            return response()->json([
+                'success' => false,
+                'message' => 'User Gagal diubah!',
+            ], 401);
         }
-
     }
 }
